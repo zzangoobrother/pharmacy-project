545f90107bc0ca4be72dda66fbe819396dcca4fa
choiseon <com1368@naver.com>
2023-01-08 14:47:22 +0900

    Initial commit

---
+++ src/main/java/com/example/pharmacyproject/PharmacyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pharmacyproject/PharmacyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
