5c092a90fb719141111c672c163813d784573adf
choiseon <com1368@naver.com>
2023-01-22 17:13:24 +0900

    feat : jpa 더티체킹, transaction 테스트

--- src/main/java/com/example/pharmacyproject/pharmacy/entity/Pharmacy.java
+++ src/main/java/com/example/pharmacyproject/pharmacy/entity/Pharmacy.java
@@ -1,5 +1,6 @@
 package com.example.pharmacyproject.pharmacy.entity;
 
+import com.example.pharmacyproject.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,7 +16,7 @@ import javax.persistence.Id;
 @AllArgsConstructor
 @Builder
 @Entity(name = "pharmacy")
-public class Pharmacy {
+public class Pharmacy extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +26,8 @@ public class Pharmacy {
     private String pharmacyAddress;
     private double latitude;
     private double longitude;
+
+    public void changePharmacyAddress(String address) {
+        this.pharmacyAddress = address;
+    }
 }
---
+++ src/main/java/com/example/pharmacyproject/pharmacy/service/PharmacyRepositoryService.java
@@ -0,0 +1,62 @@
+package com.example.pharmacyproject.pharmacy.service;
+
+import com.example.pharmacyproject.pharmacy.entity.Pharmacy;
+import com.example.pharmacyproject.pharmacy.repository.PharmacyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PharmacyRepositoryService {
+
+    private final PharmacyRepository pharmacyRepository;
+
+    public void bar(List<Pharmacy>pharmacies) {
+        log.info("bar CurrentTransactionName : " + TransactionSynchronizationManager.getCurrentTransactionName());
+        foo(pharmacies);
+    }
+
+    @Transactional
+    public void foo(List<Pharmacy> pharmacies) {
+        log.info("foo CurrentTransactionName : " + TransactionSynchronizationManager.getCurrentTransactionName());
+        pharmacies.forEach(pharmacy -> {
+            pharmacyRepository.save(pharmacy);
+            throw new RuntimeException("error");
+        });
+    }
+
+    @Transactional(readOnly = true)
+    public void startReadOnlyMethod(Long id) {
+        pharmacyRepository.findById(id).ifPresent(pharmacy -> pharmacy.changePharmacyAddress("서울 특별시 광진구"));
+    }
+
+    @Transactional
+    public void updateAddress(Long id, String address) {
+        Pharmacy pharmacy = pharmacyRepository.findById(id).orElse(null);
+
+        if (Objects.isNull(pharmacy)) {
+            log.error("[PharmacyRepositoryService updateAddress] not found id : {}", id);
+            return;
+        }
+
+        pharmacy.changePharmacyAddress(address);
+    }
+
+    public void updateAddressWithoutTransaction(Long id, String address) {
+        Pharmacy pharmacy = pharmacyRepository.findById(id).orElse(null);
+
+        if (Objects.isNull(pharmacy)) {
+            log.error("[PharmacyRepositoryService updateAddress] not found id : {}", id);
+            return;
+        }
+
+        pharmacy.changePharmacyAddress(address);
+    }
+}
